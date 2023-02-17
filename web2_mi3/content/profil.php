ee098e8eed8f59259d58ed84dfc224ed7f292dfb
yordantuharea <125641888+yordantuharea@users.noreply.github.com>
2023-02-17 14:56:13 +0700

    Add files via upload

---
+++ web2_mi3/content/profil.php
@@ -0,0 +1,16 @@
+<center>
+<img src="img/yrdnjt.png" alt="" width="200" height="200">
+<hr>
+<h1><b>YORDAN JAUHARI TUHAREA</b></h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.<br>
+    Exercitationem non pariatur aliquid quam nemo, est excepturi. Aliquam ipsa iure et odit repellat quaerat omnis quia labore vel reiciendis, temporibus nihil.<br>
+Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti laudantium, pariatur perferendis, eum vero,<br>
+minima rerum fugit a illo aliquam distinctio tempora? Totam quisquam reiciendis consectetur quasi ab velit placeat.</p>
+<br>
+</center>
+<p>Hobby :
+    <ul>
+        <li>I don't have a hobby :) </li>
+    </ul>
+</p>
+<hr>
